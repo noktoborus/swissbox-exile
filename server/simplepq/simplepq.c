295182083a0cb9e89d2d16bced02c811978daa3c
Noktoborus <noktoborus@gmail.com>
2015-04-13 17:08:44 +0300

    вынос инициализационного sql в sql/struct.sql

--- server/simplepq/simplepq.c
+++ server/simplepq/simplepq.c
@@ -573,77 +573,25 @@ spq_close()
 bool
 spq_create_tables()
 {
-	const char *const tbs[] = {"CREATE TABLE IF NOT EXISTS file_records "
-		"("
-		"	time timestamp with time zone NOT NULL DEFAULT now(), "
-		"	username varchar(1024) NOT NULL, "
-		"	chunk_hash varchar(1024) NOT NULL, "
-		"	chunk_guid UUID NOT NULL, "
-		"	rootdir_guid UUID NOT NULL, "
-		"	file_guid UUID NOT NULL, "
-		"	revision_guid UUID NOT NULL, "
-		"	chunk_path varchar(1024) NOT NULL, "
-		"	\"offset\" integer NOT NULL DEFAULT 0, "
-		"	origin integer NOT NULL DEFAULT 0 "
-		");",
-		"CREATE TABLE IF NOT EXISTS file_keys "
-		"("
-		"	time timestamp with time zone NOT NULL DEFAULT now(), "
-		"	username varchar(1024) NOT NULL,"
-		"	rootdir_guid UUID NOT NULL, "
-		"	file_guid UUID NOT NULL, "
-		"	revision_guid UUID DEFAULT NULL, "
-		"	directory_guid UUID NOT NULL, "
-		"	parent_revision_guid UUID DEFAULT NULL, "
-		"	enc_filename varchar(1024) NOT NULL, "
-		"	deviceid bigint NOT NULL, "
-		"	public_key varchar(4096) NOT NULL"
-		");",
-		"CREATE TABLE IF NOT EXISTS directory_log "
-		"("
-		"	time timestamp with time zone NOT NULL DEFAULT now(),"
-		"	username varchar(1024) NOT NULL,"
-		"	rootdir_guid UUID NOT NULL,"
-		"	directory_guid UUID NOT NULL,"
-		"	path varchar(4096) DEFAULT NULL,"
-		"	deviceid bigint NOT NULL"
-		");", /* таблица directory_tree должна заполняться автоматически
-				 по триггеру в таблице directory_log
-				 содержит текущий список каталогов
-				 */
-		"CREATE TABLE IF NOT EXISTS directory_tree (LIKE directory_log)",
-		"CREATE UNIQUE INDEX file_keys_urfr_idx "
-		"ON file_keys "
-		"("
-		"	lower(username),"
-		"	rootdir_guid,"
-		"	file_guid,"
-		"	revision_guid"
-		");",
-		"CREATE UNIQUE INDEX file_records_urfcr_idx "
-		"ON file_records "
-		"("
-		"	lower(username),"
-		"	rootdir_guid,"
-		"	file_guid,"
-		"	revision_guid,"
-		"	chunk_guid"
-		");",
-		NULL
-	};
-	char **p;
+	const char *const tb = "SELECT fepserver_installed();";
 	struct spq *sc;
 	PGresult *res;
+	ExecStatusType pqs;
 	sc = acquire_conn(&_spq);
 	if (!sc)
 		return false;
-	for (p = (char**)tbs; *p; p++) {
-		res = PQexec(sc->conn, *p);
-		if (PQresultStatus(res) != PGRES_COMMAND_OK) {
-			xsyslog(LOG_INFO, "create error: %s", PQresultErrorMessage(res));
-		}
-		PQclear(res);
+
+	res = PQexec(sc->conn, tb);
+	pqs = PQresultStatus(res);
+	if (pqs != PGRES_TUPLES_OK) {
+		xsyslog(LOG_ERR, "postgresql: %s", PQresultErrorMessage(res));
+		xsyslog(LOG_ERR, "please inject sql/struct.sql into db");
+		release_conn(&_spq, sc);
+		return false;
+	} else {
+		xsyslog(LOG_INFO, "db version: %s", PQgetvalue(res, 0, 0));
 	}
+	PQclear(res);
 
 	release_conn(&_spq, sc);
 	return true;
--- server/sql/struct.sql
+++ server/sql/struct.sql
@@ -1,13 +1,87 @@
 /*	vim: synta=pgsql
 */
 
-CREATE OR REPLACE FUNCTION directory_action()
-RETURNS TRIGGER AS $$
+DROP TABLE IF EXISTS file_recrods;
+CREATE TABLE IF NOT EXISTS file_records
+(
+	time timestamp with time zone NOT NULL DEFAULT now(),
+	username varchar(1024) NOT NULL,
+	chunk_hash varchar(1024) NOT NULL,
+	chunk_guid UUID NOT NULL,
+	rootdir_guid UUID NOT NULL,
+	file_guid UUID NOT NULL,
+	revision_guid UUID NOT NULL,
+	chunk_path varchar(1024) NOT NULL,
+	"offset" integer NOT NULL DEFAULT 0,
+	origin integer NOT NULL DEFAULT 0
+);
+
+DROP TABLE IF EXISTS file_keys;
+CREATE TABLE IF NOT EXISTS file_keys
+(
+	time timestamp with time zone NOT NULL DEFAULT now(),
+	username varchar(1024) NOT NULL,
+	rootdir_guid UUID NOT NULL,
+	file_guid UUID NOT NULL,
+	revision_guid UUID DEFAULT NULL,
+	directory_guid UUID NOT NULL,
+	parent_revision_guid UUID DEFAULT NULL,
+	enc_filename varchar(1024) NOT NULL,
+	deviceid bigint NOT NULL,
+	public_key varchar(4096) NOT NULL
+);
+
+DROP TABLE IF EXISTS directory_log CASCADE;
+CREATE TABLE IF NOT EXISTS directory_log
+(
+	time timestamp with time zone NOT NULL DEFAULT now(),
+	username varchar(1024) NOT NULL,
+	rootdir_guid UUID NOT NULL,
+	directory_guid UUID NOT NULL,
+	path varchar(4096) DEFAULT NULL,
+	deviceid bigint NOT NULL
+);
+
+/* таблица directory_tree должна заполняться автоматически
+   	 по триггеру в таблице directory_log
+   	 содержит текущий список каталогов
+   	 */
+DROP TABLE IF EXISTS directory_tree;
+CREATE TABLE IF NOT EXISTS directory_tree (LIKE directory_log);
+
+
+CREATE UNIQUE INDEX file_keys_urfr_idx
+ON file_keys
+(
+	lower(username),
+	rootdir_guid,
+	file_guid,
+	revision_guid
+);
+
+CREATE UNIQUE INDEX file_records_urfcr_idx
+ON file_records
+(
+	lower(username),
+	rootdir_guid,
+	file_guid,
+	revision_guid,
+	chunk_guid
+);
+
+CREATE OR REPLACE FUNCTION fepserver_installed()
+	RETURNS text AS $$
+DECLARE
+	_retval text;
 BEGIN
-	RAISE NOTICE 'path %', NEW.path;
+	SELECT INTO _retval version();
+	RETURN _retval;
+END $$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION directory_action()
+	RETURNS TRIGGER AS $$
+BEGIN
 	IF NEW.path IS NULL THEN
-		RAISE NOTICE 'SELECT FROM directory_tree WHERE username = % AND rootdir_guid = % AND directory_guid = %;',
-			NEW.username, NEW.rootdir_guid, NEW.directory_guid;
 		DELETE FROM directory_tree
 		WHERE
 			username = NEW.username AND
