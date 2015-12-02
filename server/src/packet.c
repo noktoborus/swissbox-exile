52fd33e862434d2054a6efb09f1857966677a8aa
Noktoborus <noktoborus@gmail.com>
2015-12-02 16:17:16 +0300

    добавлено отображение всех пакетов в лог

--- server/src/packet.c
+++ server/src/packet.c
@@ -8,6 +8,7 @@
 #include <stdint.h>
 #include <string.h>
 
+#include "junk/guid.h"
 #include "junk/utils.h"
 #include "junk/xsyslog.h"
 #include "proto/fep.pb-c.h"
@@ -27,6 +28,23 @@
 	if (len > 0u && s->field)\
 		begin += snprintf(result + begin, (len -= begin),\
 			", "#field"=\"%s\"", s->field);\
+}
+
+#define _value_or_nullB(s, result, field, Xbegin, Xlen) \
+{\
+	if (Xlen > 0u && s->has_##field) {\
+		if (s->field.len) {\
+			size_t ___l = s->field.len * 2;\
+			char *___e = calloc(1, ___l + 1);\
+			bin2hex(s->field.data, s->field.len, ___e, ___l);\
+			Xbegin += snprintf(result + Xbegin, (Xlen -= Xbegin),\
+					", "#field"=\"%s\"", ___e);\
+			free(___e);\
+		} else {\
+			Xbegin += snprintf(result + Xbegin, (Xlen -= Xbegin),\
+					", "#field"=?");\
+		}\
+	}\
 }
 
 static enum packet_verbose _packet_verbose[FEP__TYPE__t_max];
@@ -211,6 +229,86 @@ packet2syslog(const char *head,
 		return;
 
 	switch (type) {
+	case FEP__TYPE__tReqAuth:
+		{
+			Fep__ReqAuth *_m = (void*)msg;
+			char _epoch[GUID_MAX + 1];
+			bin2hex(_m->epoch_guid.data, _m->epoch_guid.len, PSIZE(_epoch));
+			_l = snprintf(result, _e, "id=%"PRIu64", epoch_guid=%s",
+					_m->id, _epoch);
+			_value_or_nullS(_m, result, text, _l, _e);
+			break;
+
+		}
+	case FEP__TYPE__tAuth:
+		{
+			Fep__Auth *_m = (void*)msg;
+			snprintf(result, _e,
+					"id=%"PRIu64", device_id=%"PRIX64", domain=%s, authType=%s",
+					_m->id, _m->device_id, _m->domain,
+					(_m->authtype == FEP__REQ_AUTH_TYPE__tHash
+					 ? "Hash" :
+					 (_m->authtype == FEP__REQ_AUTH_TYPE__tUserPassword
+					  ? "UserPassword" :
+					  (_m->authtype == FEP__REQ_AUTH_TYPE__tUserToken
+					   ? "UserToken" :
+					   (_m->authtype == FEP__REQ_AUTH_TYPE__tKey
+						? "Key": "Unknown"))))
+					);
+			break;
+		}
+	case FEP__TYPE__tWriteAsk:
+		{
+			Fep__WriteAsk *_m = (void*)msg;
+			char _hash[HASHHEX_MAX + 1];
+			memset(_hash, 0u, sizeof(_hash));
+			bin2hex(_m->chunk_hash.data, _m->chunk_hash.len,
+					_hash, sizeof(_hash));
+			snprintf(result, _e,
+					"id=%"PRIu64
+					", rootdir_guid=%s, file_guid=%s, chunk_guid=%s"
+					", revision_guid=%s, chunk_hash=%s"
+					", size=%"PRIu32", offset=%"PRIu32,
+					_m->id, _m->rootdir_guid, _m->file_guid, _m->chunk_guid,
+					_m->revision_guid, _hash, _m->size, _m->offset);
+			break;
+		}
+	case FEP__TYPE__tOk:
+		{
+			Fep__Ok *_m = (void*)msg;
+			_l = snprintf(result, _e, "id=%"PRIu64, _m->id);
+			_value_or_nullS(_m, result, message, _l, _e);
+			break;
+		}
+	case FEP__TYPE__tFileMeta:
+		{
+			Fep__FileMeta *_m = (void*)msg;
+			_l = snprintf(result, _e,
+					"id=%"PRIu64
+					", rootdir_guid=%s, file_guid=%s, revision_guid=%s"
+					", directory_guid=%s, chunks=%"PRIu32,
+					_m->id, _m->rootdir_guid, _m->file_guid, _m->revision_guid,
+					_m->directory_guid, _m->chunks);
+			_value_or_nullS(_m, result, parent_revision_guid, _l, _e);
+			_value_or_nullS(_m, result, enc_filename, _l, _e);
+			_value_or_nullB(_m, result, key, _l, _e);
+			break;
+		}
+	case FEP__TYPE__tPing:
+		{
+			Fep__Ping *_m = (void*)msg;
+			snprintf(result, _e, "id=%"PRIu64", sec=%"PRIu64", usec=%"PRIu32,
+					_m->id, _m->sec, _m->usec);
+			break;
+		}
+	case FEP__TYPE__tPong:
+		{
+			Fep__Pong *_m = (void*)msg;
+			snprintf(result, _e, "id=%"PRIu64", sec=%"PRIu64", usec=%"PRIu32
+					", peer_sec=%"PRIu64", peer_usec=%"PRIu32,
+					_m->id, _m->sec, _m->usec, _m->peer_sec, _m->peer_usec);
+			break;
+		}
 	case FEP__TYPE__tError:
 		{
 			Fep__Error *_m = (void*)msg;
@@ -272,6 +370,162 @@ packet2syslog(const char *head,
 					"id=%"PRIu64", checkpoint=%"PRIu64", session_id=%"PRIu32,
 					_m->id, _m->checkpoint, _m->session_id);
 			_value_or_nullS(_m, result, rootdir_guid, _l, _e);
+			break;
+		}
+	case FEP__TYPE__tEnd:
+		{
+			Fep__End *_m = (void*)msg;
+			_l = snprintf(result, _e,
+					"id=%"PRIu64", session_i=%"PRIu32, _m->id, _m->session_id);
+			_value_or_null(_m, result, "%"PRIu32, packets, _l, _e);
+			break;
+		}
+	case FEP__TYPE__tOkWrite:
+		{
+			Fep__OkWrite *_m = (void*)msg;
+			_l = snprintf(result, _e,
+					"id=%"PRIu64", session_id=%"PRIu32, _m->id, _m->session_id);
+			break;
+		}
+	case FEP__TYPE__tOkRead:
+		{
+			Fep__OkRead *_m = (void*)msg;
+			_l = snprintf(result, _e,
+					"id=%"PRIu64", session_id=%"PRIu32
+					", size=%"PRIu32", offset=%"PRIu32,
+					_m->id, _m->session_id, _m->size, _m->offset);
+			break;
+		}
+	case FEP__TYPE__txfer:
+		{
+			Fep__Xfer *_m = (void*)msg;
+			_l = snprintf(result, _e,
+					"id=%"PRIu64", session_id=%"PRIu32
+					", offset=%"PRIu64", data.len=%"PRIu64,
+					_m->id, _m->session_id,
+					_m->offset, _m->data.len);
+			break;
+		}
+	case FEP__TYPE__tReadAsk:
+		{
+			Fep__ReadAsk *_m = (void*)msg;
+			_l = snprintf(result, _e,
+					"id=%"PRIu64
+					", rootdir_guid=%s, file_guid=%s, chunk_guid=%s",
+					_m->id,
+					_m->rootdir_guid, _m->file_guid, _m->chunk_guid);
+			break;
+		}
+	case FEP__TYPE__tQueryRevisions:
+		{
+			Fep__QueryRevisions *_m = (void*)msg;
+			_l = snprintf(result, _e,
+					"id=%"PRIu64", session_id=%"PRIu32
+					", rootdir_guid=%s, file_guid=%s, depth=%"PRIu32,
+					_m->id, _m->session_id,
+					_m->rootdir_guid, _m->file_guid, _m->depth);
+			break;
+		}
+	case FEP__TYPE__tResultRevision:
+		{
+			Fep__ResultRevision *_m = (void*)msg;
+			_l = snprintf(result, _e,
+					"id=%"PRIu64", session_id=%"PRIu32
+					", no=%"PRIu32", max=%"PRIu32", revision_guid=%s",
+					_m->id, _m->session_id,
+					_m->rev_no, _m->rev_max, _m->revision_guid);
+			_value_or_nullS(_m, result, parent_revision_guid, _l, _e);
+			break;
+		}
+	case FEP__TYPE__tQueryChunks:
+		{
+			Fep__QueryChunks *_m = (void*)msg;
+			_l = snprintf(result, _e,
+				"id=%"PRIu64", session_id=%"PRIu32
+				", rootdir_guid=%s, file_guid=%s, revision_guid=%s",
+				_m->id, _m->session_id,
+				_m->rootdir_guid, _m->file_guid, _m->revision_guid);
+			break;
+		}
+	case FEP__TYPE__tResultChunk:
+		{
+			Fep__ResultChunk *_m = (void*)msg;
+			char _hash[HASHHEX_MAX + 1];
+			memset(_hash, 0, sizeof(_hash));
+			bin2hex(_m->chunk_hash.data, _m->chunk_hash.len,
+					_hash, sizeof(_hash));
+			_l = snprintf(result, _e,
+				"id=%"PRIu64", session_id=%"PRIu32
+				", no=%"PRIu32", max=%"PRIu32", chunk_guid=%s, chunk_hash=%s",
+				_m->id, _m->session_id,
+				_m->chunk_no, _m->chunk_max,
+				_m->chunk_guid, _hash);
+
+			break;
+		}
+	case FEP__TYPE__tRenameChunk:
+		{
+			Fep__RenameChunk *_m = (void*)msg;
+			_l = snprintf(result, _e,
+				"id=%"PRIu64
+				", rootdir_guid=%s, file_guid=%s, chunk_guid=%s"
+				", to_chunk_guid=%s, to_revision_guid=%s",
+				_m->id,
+				_m->rootdir_guid, _m->file_guid, _m->chunk_guid,
+				_m->to_chunk_guid, _m->to_revision_guid);
+			break;
+		}
+	case FEP__TYPE__tOkUpdate:
+		{
+			Fep__OkUpdate *_m = (void*)msg;
+			_l = snprintf(result, _e,
+					"id=%"PRIu64", checkpoint=%"PRIu64,
+					_m->id, _m->checkpoint);
+			_value_or_nullS(_m, result, message, _l, _e);
+			break;
+		}
+	case FEP__TYPE__tChat:
+		{
+			Fep__Chat *_m = (void*)msg;
+			_l = snprintf(result, _e,
+					"id=%"PRIu64", device_id_from=%"PRIu64
+					"message.len=%"PRIuPTR,
+					_m->id, _m->device_id_from,
+					_m->message.len);
+			_value_or_null(_m, result, "%"PRIu64, device_id_to, _l, _e);
+			_value_or_nullS(_m, result, user_to, _l, _e);
+			break;
+		}
+	case FEP__TYPE__tState:
+		{
+			Fep__State *_m = (void*)msg;
+			_l = snprintf(result, _e, "id=%"PRIu64, _m->id);
+			_value_or_null(_m, result, "%"PRIu32, devices, _l, _e);
+			_value_or_null(_m, result, "%"PRIu64, last_auth_device, _l, _e);
+			_value_or_nullS(_m, result, last_auth_time, _l, _e);
+			_value_or_nullS(_m, result, last_auth_addr, _l, _e);
+			break;
+		}
+	case FEP__TYPE__tQueryDevices:
+		{
+			Fep__QueryDevices *_m = (void*)msg;
+			_l = snprintf(result, _e,
+					"id=%"PRIu64", session_id=%"PRIu32,
+					_m->id, _m->session_id);
+			break;
+		}
+	case FEP__TYPE__tResultDevice:
+		{
+			Fep__ResultDevice *_m = (void*)msg;
+			_l = snprintf(result, _e,
+				"id=%"PRIu64", session_id=%"PRIu32
+				", no=%"PRIu32", max=%"PRIu32
+				", device_id=%"PRIX64", is_online=%s"
+				", last_auth_time=%s",
+				_m->id, _m->session_id,
+				_m->dev_no, _m->dev_max, _m->device_id,
+				(_m->is_online ? "true" : "false"),
+				_m->last_auth_time);
 			break;
 		}
 	default:
