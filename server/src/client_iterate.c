c25f8a4f1a92ca01b834673b857790dc676afd58
Noktoborus <noktoborus@gmail.com>
2016-01-11 13:52:04 +0300

    обвноление комментариев

--- server/src/client_iterate.c
+++ server/src/client_iterate.c
@@ -643,7 +643,7 @@ client_load(struct client *c)
 		c->cev->pain->options.limit_local_sql_queries;
 	c->options.limit_local_fd_queries =
 		c->cev->pain->options.limit_local_fd_queries;
-
+	/* FIXME: при массовой перезагрузке конфига можно опрокинуться */
 	return send_ping(c);
 }
 
