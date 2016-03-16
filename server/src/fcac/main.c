8209cfbabf9a45769951a7652f794f50a2e6c414
Noktoborus <noktoborus@gmail.com>
2016-03-16 13:02:24 +0300

    fcac: актуализация кода

--- server/src/fcac/main.c
+++ server/src/fcac/main.c
@@ -1,6 +1,6 @@
 /* vim: ft=c ff=unix fenc=utf-8
  * file: src/fcac/main.c
- * ${CC} -o fcac -I../../ -I../ -std=c99 -ggdb2 -D_DEFAULT_SOURCE main.c fcac.c ../../junk/utils.c -lpthread
+ * ${CC} -o fcac -I../../ -I../ -std=c99 -ggdb2 -DFCAC_DEEPDEBUG -D_DEFAULT_SOURCE main.c fcac.c ../../junk/utils.c -lpthread
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,6 +9,9 @@
 #include <string.h>
 #include <unistd.h>
 
+#include <sys/stat.h>
+#include <sys/types.h>
+
 #include "junk/xsyslog.h"
 #include "fcac.h"
 
@@ -16,27 +19,38 @@ int
 main(int argc, char *argv[])
 {
 	struct fcac fcac = {0};
-	struct fcac_ptr fptr = {0};
+	const char fcac_path[] = "./fcac_data";
+
+	struct fcac_ptr fptr[10] = {0};
+	const size_t count = sizeof(fptr) / sizeof(struct fcac_ptr);
+
 	openlog(NULL, LOG_PERROR | LOG_PID, LOG_LOCAL0);
+	mkdir(fcac_path, S_IRWXU);
 	fcac_init(&fcac, true);
-	fcac_set(&fcac, FCAC_PATH, PSLEN_S("./fcac_data"));
+	fcac_set(&fcac, FCAC_PATH, PSLEN_S(fcac_path));
 	fcac_set(&fcac, FCAC_MAX_MEM_SIZE, 19);
-	fcac_set(&fcac, FCAC_TIME_EXPIRE, (time_t)1);
+	/*fcac_set(&fcac, FCAC_TIME_EXPIRE, (time_t)1);*/
 
-	fcac_open(&fcac, 1, &fptr);
+	for (size_t i = 0u; i < count; i++) {
+		fcac_open(&fcac, i, fptr + i, 0);
+	}
 
-	if (fcac_is_ready(&fptr) == FCAC_READY) {
-		fprintf(stderr, "ready\n");
-	} else {
-		fprintf(stderr, "write: %"PRIuPTR"\n", fcac_write(&fptr, "123456789", 9));
-		fprintf(stderr, "write: %"PRIuPTR"\n", fcac_write(&fptr, "123456789", 9));
-		fprintf(stderr, "write: %"PRIuPTR"\n", fcac_write(&fptr, "123456789", 9));
-		fcac_set_ready(&fptr);
+	for (size_t i = 0u; i < count; i++) {
+		if (fcac_is_ready(fptr + i) == FCAC_READY) {
+			fprintf(stderr, "ready\n");
+		} else {
+			fprintf(stderr, "write: %"PRIuPTR"\n", fcac_write(fptr + i, "123456789", 9));
+			fprintf(stderr, "write: %"PRIuPTR"\n", fcac_write(fptr + i, "123456789", 9));
+			fprintf(stderr, "write: %"PRIuPTR"\n", fcac_write(fptr + i, "123456789", 9));
+			fcac_set_ready(fptr + i);
+		}
+		fcac_close(fptr + i);
 	}
 
 	fcac_tick(&fcac);
-	fcac_close(&fptr);
-	sleep(2);
+
+	fcac_tick(&fcac);
+
 	fcac_tick(&fcac);
 
 	fcac_destroy(&fcac);
